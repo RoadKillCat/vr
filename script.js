38ea6be461e8c5abb1cf1599ccc974dd705452b2
roadkillcat <joedominciddon@gmail.com>
2018-04-01 15:24:01 +0100

    :)

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var msg = document.getElementById('msg');
 var cnvs_l = document.getElementById('cnvs_l');
 var cnvs_r = document.getElementById('cnvs_r');
 var cnvii = document.getElementById('cnvii');
@@ -8,17 +7,6 @@ var cnvii = document.getElementById('cnvii');
 function fts(){
     cnvs_l.width = cnvs_r.width = innerWidth / 2;
     cnvs_l.height = cnvs_r.height = innerHeight;
-}
-
-screen.orientation.onchange = function(){
-	if (/landscape/.test(screen.orientation.type)){
-		cnvii.style.display = 'inline';
-		msg.style.display = 'none';
-		setTimeout(fts, 500);
-    } else {
-		cnvii.style.display = 'none';
-        msg.style.display = 'block';
-   }
 }
 
 function block(col){
