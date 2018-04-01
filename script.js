1bded3fe65f94ce70aceb58b19df3e7f9e5822eb
roadkillcat <joedominciddon@gmail.com>
2018-04-01 10:37:14 +0100

    rm swp

--- script.js
+++ script.js
@@ -21,8 +21,6 @@ screen.orientation.onchange = function(){
    }
 }
 
-fts();
-
 function block(col){
     return [{verts: [{x: 0, y: 0, z: 0}, {x: 1, y: 0, z: 0}, {x: 1, y: 1, z: 0}, {x: 0, y: 1, z: 0}], col: col},
             {verts: [{x: 0, y: 0, z: 0}, {x: 0, y: 1, z: 0}, {x: 0, y: 1, z: 1}, {x: 0, y: 0, z: 1}], col: col},
