5410163ff1cab2bec35165b57a38db1ed25d63f5
roadkillcat <joedominciddon@gmail.com>
2018-04-01 10:31:53 +0100

    added msg

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var msg = document.getElementById('msg');
 var cnvs_l = document.getElementById('cnvs_l');
 var cnvs_r = document.getElementById('cnvs_r');
 var cnvii = document.getElementById('cnvii');
@@ -7,6 +8,17 @@ var cnvii = document.getElementById('cnvii');
 function fts(){
     cnvs_l.width = cnvs_r.width = innerWidth / 2;
     cnvs_l.height = cnvs_r.height = innerHeight;
+}
+
+screen.orientation.onchange = function(){
+	if (/landscape/.test(screen.orientation.type)){
+		cnvii.style.display = 'inline';
+		msg.style.display = 'none';
+		fts();
+    } else {
+		cnvii.style.display = 'none';
+        msg.stlye.display = 'block';
+    }
 }
 
 fts();
