9bd3bc60c53156062007452a9d24dc1f4f2c2a9e
roadkillcat <joedominciddon@gmail.com>
2018-04-02 14:54:07 +0100

    wrong var names corrected

--- script.js
+++ script.js
@@ -43,10 +43,10 @@ function update(time){
 
 function take_step(angle){
     let next = {x: cam.step * Math.sin(zengine.to_rad(angle)),
-            y: cam.step * Math.cos(zengine.to_rad(angle))};
+                y: cam.step * Math.cos(zengine.to_rad(angle))};
     let blk = {x: Math.floor(next.x),
                y: Math.floor(next.y)}
-    if (blk.x < 0 || blk.y < 0 || blk.x > mazeWidth * 2 + 1 || blk.y > mazeHeight * 2 + 1 || !maze[blk.y][blk.x]){
+    if (blk.x < 0 || blk.y < 0 || blk.x > maze_width * 2 + 1 || blk.y > maze_height * 2 + 1 || !maze[blk.y][blk.x]){
         cam.x = next.x
         cam.y = next.y
     }
